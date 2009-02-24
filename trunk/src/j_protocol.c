fb89f13d4dcc261622a6df140827e1a8f3627c40
notargiacomo.s <notargiacomo.s@9e400658-cb68-11dd-8ca8-e3ff79f713b6>
2009-02-24 22:56:51 +0000

    fix

--- trunk/src/j_protocol.c
+++ trunk/src/j_protocol.c
@@ -2,11 +2,12 @@
 
 j_packet *j_create_packet(u_int1 type, u_int1 data[J_DATA_LEN], u_int1 len) {
 	j_packet *packet = (j_packet*)calloc(1, sizeof(j_packet));
-	packet->id = rand();
+	packet->start = J_START_CODE;
 	packet->type = type;
 	packet->len = len;
 	memset(packet->data, 0, J_DATA_LEN);
 	memcpy(packet->data, data, len);
+	packet->end = J_END_CODE;
 	return packet;
 }
 
@@ -38,11 +39,31 @@ char *j_recv(int socket, int *len) {
 	recvQueue = g_queue_new();
 	g_queue_init(recvQueue);
 	char *data = calloc(J_LEN, 1);
+	char *packet = calloc(J_LEN, 1);
 	*len = 0;
 	int n_read;
-	while((n_read=recv(socket, data, J_LEN, J_FLAGS))>0) {
+	
+	printf("[j_recv] Enter\n");
+//	while((n_read=recv(socket, data, J_LEN, J_FLAGS))>0) {
+	while((n_read=read(socket, data, J_LEN))>0) {
 		printf("[j_recv] Received: %s\n", hex_dump(data, n_read, 10));
+		if(data[0]==J_START_CODE && n_read<J_PACKET_LEN) {
+			memcpy(packet, data, n_read);
+			*len = n_read;
+		}
+		else if(data[0]==J_START_CODE && n_read==J_PACKET_LEN && data[n_read-1]==J_END_CODE) {
+			memcpy(packet, data, n_read);
+			*len = n_read;
+		}
+		else if(data[0]!=J_START_CODE && *len<J_PACKET_LEN) {
+			char *iter = packet;
+			iter += *len;
+			memcpy(iter, data, n_read);
+			*len += n_read;
+		}
+		printf("[j_recv] Received: %s\n", hex_dump(packet, *len, 10));
 		//g_queue_push_tail(recvQueue, (gpointer)data);
 	}
+	printf("[j_recv] Exit\n");
 	return data;
 }
--- trunk/src/j_protocol.h
+++ trunk/src/j_protocol.h
@@ -10,9 +10,11 @@
 #include "utils.h"
 
 #define J_FLAGS			0
-#define J_LEN			1
-#define J_DATA_LEN		16
-#define J_PACKET_LEN		3+J_DATA_LEN
+#define J_DATA_LEN		7
+#define J_PACKET_LEN		4+J_DATA_LEN
+#define J_LEN			J_PACKET_LEN
+#define J_START_CODE		0x55
+#define J_END_CODE		0x66
 
 /*
  *    Johnny Packet
@@ -21,10 +23,11 @@
  * +----+------+-----+-----+
  */
 struct j_packet {
-	u_int1	id;
+	u_int1	start;
 	u_int1	type;
 	u_int1	len;
 	u_int1	data[J_DATA_LEN];
+	u_int1	end;
 };
 typedef struct j_packet j_packet;
 
